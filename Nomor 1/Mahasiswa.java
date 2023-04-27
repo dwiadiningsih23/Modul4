700aa4ba3374561721487e2b1c2e82be59833656
dwiadiningsih23 <131975031+dwiadiningsih23@users.noreply.github.com>
2023-04-27 20:13:56 +0700

    Add files via upload

---
+++ Nomor 1/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg13020210021_dwi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mahasiswa {
+      private String Nama;
+    private long Stb;   
+public String getNama(){
+    return this.Nama;
+}
+public void setNama(String Nama){
+    this.Nama = Nama;
+}
+public long getStb(){
+    return this.Stb;
+}
+public void setStb(long Stb){
+    this.Stb = Stb;
+}
+}
---
+++ Nomor 1/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg13020210021_dwi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa Mahasiswa = new Mahasiswa();
+       
+       Mahasiswa.setNama("Dwi Adiningsih");
+       Mahasiswa.setStb(13020210021L);
+        System.out.println(Mahasiswa.getNama());
+        System.out.println(Mahasiswa.getStb());
+    
+    }
+    
+    
+}
